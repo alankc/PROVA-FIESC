2eceece292663043f43f8cabd44e8bd4c810bdb6
alan <cechinel.a.k@gmail.com>
2023-02-28 13:33:58 -0300

    comentários extras

--- Q2/test.cpp
+++ Q2/test.cpp
@@ -7,9 +7,9 @@ int main()
     b.add(1);
     b.add(2);
     b.add(3);
-    b.add(35);
+    b.add(35); //não adiciona, buffer cheio
 
-    auto itens = b.show();
+    auto itens = b.show(); // printa: 1 2 3
     for (auto item : itens)
     {
         cout << item << " ";
@@ -17,18 +17,19 @@ int main()
     cout << endl;
 
     int removed;
-    if (b.remove(removed))
+    if (b.remove(removed)) // remove o 1
         cout << "R " << removed << endl;
 
-    if (b.remove(removed))
+    if (b.remove(removed)) // remove o 2
         cout << "R " << removed << endl;
 
-    if (b.remove(removed))
+    if (b.remove(removed)) // remove o 3
         cout << "R " << removed << endl;
 
-    if (b.remove(removed))
+    if (b.remove(removed)) //Deve dar o erro avisando que o buffer está vazio
         cout << "R " << removed << endl;
 
+    //Deve aparecer apenas o 35
     b.add(35);
     itens = b.show();
     for (auto item : itens)
